e0501567d83b0b075a0b7456fe375c573f9a981a
ciao0603 <jocelyn94032.0@gmail.com>
2023-09-29 17:24:30 +0800

    feat: add getRestaurants api

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const methodOverride = require('method-override')
 const hbsHelpers = require('./helpers/hbs-helpers')
 const passport = require('./config/passport')
 const { getUser } = require('./helpers/auth-helpers')
-const { pages } = require('./routes')
+const { pages, apis } = require('./routes')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -40,6 +40,7 @@ app.use((req, res, next) => {
   next()
 })
 
+app.use('/api', apis)
 app.use(pages)
 
 app.listen(port, () => {
