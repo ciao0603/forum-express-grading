09a609452c6a10b7019c0f45eb2be8ead292ff63
ciao0603 <jocelyn94032.0@gmail.com>
2023-09-30 11:34:27 +0800

    feat: add api error handler & delete restaurant api

--- controllers/apis/admin-controller.js
+++ controllers/apis/admin-controller.js
@@ -3,6 +3,9 @@ const adminServices = require('../../services/restaurant-service')
 const adminController = {
   getRestaurants: (req, res, next) => {
     adminServices.getRestaurants(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+  },
+  deleteRestaurant: (req, res, next) => {
+    adminServices.deleteRestaurant(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   }
 }
 
